2fdd31cdd26b7bf896fc4e9836deede1b35a082b
ljs3536 <wlstjr320@naver.com>
2024-11-06 21:30:13 +0900

    Validator 분리1

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -26,6 +26,8 @@ public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
 
+    private final ItemValidator itemValidator;
+
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
@@ -154,7 +156,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         log.info("objectName={}", bindingResult.getObjectName());
@@ -179,6 +181,23 @@ public class ValidationItemControllerV2 {
             }
         }
 
+        //검증에 실패하면 다시 입력폼으로
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        itemValidator.validate(item, bindingResult);
         //검증에 실패하면 다시 입력폼으로
         if(bindingResult.hasErrors()){
             log.info("errors = {}", bindingResult);
